f613a1530e7c1e7f6df27eafdfddde82a923b6ec
Ibrahim1744 <himanaser86@gmail.com>
2023-03-13 04:11:09 +0200

    fix routing problem in next

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic"; // this is the fix
 import Results from "@/components/Results";
 import Image from "next/image";
 
@@ -7,7 +8,7 @@ export default async function Home({ searchParams }) {
   const res = await fetch(
     `https://api.themoviedb.org/3/${
       genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
-    }?api_key=${API_KEY}&language=en-US&page=1` , {next:{revalidate:10000}});
+    }?api_key=${API_KEY}&language=en-US&page=1`);
     if(!res.ok){
       throw new Error("Failed to fetch data from api")
     }
